4a72a109d8ab52591aab21f7ffaca5ab9616c60a
WarriorMachine <51814677+WarriorMachine@users.noreply.github.com>
2019-06-22 22:49:04 +0200

    Update scaleOptions.py

--- scaleOptions.py
+++ scaleOptions.py
@@ -1,6 +1,6 @@
 class scaleOptions:
     def __init__(self, activebackground=None, bd=0, bg=None, bordercolor=None, cursor=None, command=None, digits=None, font=None, fg=None, from_=0, height=250, highlightbackground=None, 
-    highlightcolor=None, label=None,orient=VERTICAL, page=main, relief=FLAT, repeatdelay=None, resolution=1 ,showvalue=1, sliderlength=30, state=None, takefocus=None, tickinterval=10, to=100, troughcolor=None,
+    highlightcolor=None, highlightthickness=None, label=None,orient=VERTICAL, page=main, relief=FLAT, repeatdelay=None, resolution=1 ,showvalue=1, sliderlength=30, state=None, takefocus=None, tickinterval=10, to=100, troughcolor=None,
     variable=None, width=75, sliderwidth=15, x=0, y=0):
         backFrameScaleOptions = Frame(page, bg=bg)
         backFrameScaleOptions.place(x=x, y=y, width=width, height=height)
@@ -8,7 +8,7 @@ class scaleOptions:
         highlightbackground=highlightbackground, highlightcolor=highlightcolor, label=label, orient=orient, relief=relief, repeatdelay=repeatdelay, resolution=resolution, showvalue=showvalue, 
         sliderlength=sliderlength, state=state, takefocus=takefocus, tickinterval=tickinterval, to=to, troughcolor=troughcolor, variable=variable, width=sliderwidth)
         echelle.place(x=0, y=0, width=width, height=height)
-        if bordercolor == None or bordercolor == 0:
+        if bordercolor == None or bordercolor == 0 or highlightthickness == 0:
             bordercolor = bg
         maskechelle1 = Frame(backFrameScaleOptions, bd=0, bg=bordercolor)
         maskechelle1.place(x=0, y=0, width=width, height=2)
