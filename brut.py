e20528bb295423c2e5019a3dc5e812675a565bec
WarriorMachine <51814677+WarriorMachine@users.noreply.github.com>
2019-09-23 17:41:44 +0200

    Update brut.py

--- brut.py
+++ brut.py
@@ -1,15 +1,18 @@
 class Brut:
     def __init__(self, characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789/@?!:;.,~-", min=1, max=4, infos=1, wait_info=1):
-        if infos==1:
-            start_time = datetime.now()
-        for n in range(min, max+1):
-            for i in itertools.product(characters, repeat=n):
-                chars = ''.join(i)
-                print(chars)
-        if infos==1:
-            end_time = datetime.now()
-            print ('\n[BRUT] A commencer le : '+str(start_time))
-            print ('\n[BRUT] Fini le : '+str(end_time))	
-            print ('\n[BRUT] Temps total : '+str(end_time-start_time))
-            if wait_info == 1:
-                time.sleep(10)
+        if min < max:
+            if infos==1:
+                start_time = datetime.now()
+            for n in range(min, max+1):
+                for i in itertools.product(characters, repeat=n):
+                    chars = ''.join(i)
+                    print(chars)
+            if infos==1:
+                end_time = datetime.now()
+                print ('\n[BRUT] A commencer le : '+str(start_time))
+                print ('\n[BRUT] Fini le : '+str(end_time))	
+                print ('\n[BRUT] Temps total : '+str(end_time-start_time))
+                if wait_info == 1:
+                    time.sleep(10)
+        else:
+            print("\n[BRUT] Erreur 0001 : La valeur min doit être inférieur ou égal à la valeur max")
