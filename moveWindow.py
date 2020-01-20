2c92b98bcdc56ec6f1100bc57a1186841a873f4b
WarriorMachine <51814677+WarriorMachine@users.noreply.github.com>
2020-01-20 17:11:33 +0100

    Update moveWindow.py

--- moveWindow.py
+++ moveWindow.py
@@ -6,8 +6,8 @@ main = Tk()
 main.overrideredirect(1)
 
 class Window_R():
-    def __init__(self, width=1360, height= 768, princippage=main, resizebarpage=main, sizebar=3, bgbar='white', bgcornerbar='gray99'):
-        maskechelle1 = Frame(resizebarpage, bd=0, bg=bgbar, cursor="size_ns")
+    def __init__(self, width=1360, height= 768, princippage=main, resizebarpage=main, sizebar=3, bgbar='white'):
+        maskechelle1 = Frame(resizebarpage, bd=0, bg=bgbar, cursor="hand2") #hand2
         maskechelle1.place(x=0, y=0, width=width, height=sizebar)
         while 1:
             mouseEvent = win32api.GetKeyState(0x01) #etat du clique gauche
@@ -15,13 +15,13 @@ class Window_R():
             y = princippage.winfo_rooty() #y = sH
             mouseX = princippage.winfo_pointerx()
             mouseY = princippage.winfo_pointery()
-            print(str(x)+" "+str(y)+" "+str(mouseX)+" "+str(mouseY))
             time.sleep(0.01)
             if mouseY>=y and mouseY<=y+sizebar-1 and mouseX>=x and mouseX<=x+width:
                 if mouseEvent == -128 or mouseEvent == -127:
                     while 1:
                         mouseX = princippage.winfo_pointerx()
                         mouseY = princippage.winfo_pointery()
+                        x = princippage.winfo_rootx()
                         newdim = str(width)+"x"+str(height)+"+"+str(mouseX-round(width/2))+"+"+str(mouseY)
                         princippage.geometry(newdim)
                         mouseEvent = win32api.GetKeyState(0x01)
@@ -39,6 +39,6 @@ ss=1440
 st=700
 main.geometry(str(ss)+"x"+str(st)+"+"+str(100)+"+"+str(220))
 
-Window_R(princippage=main,sizebar=10,height=st,width=ss, bgbar='cyan', bgcornerbar='blue')
+Window_R(princippage=main,sizebar=10,height=st,width=ss, bgbar='crimson')
 
 main.mainloop()
