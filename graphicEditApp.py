77f0a10b85fbcbd2ad77087f9d6d75bc91acdda1
WarriorMachine <51814677+WarriorMachine@users.noreply.github.com>
2020-01-24 17:26:19 +0100

    Update graphicEditApp.py

--- graphicEditApp.py
+++ graphicEditApp.py
@@ -3,16 +3,15 @@
 from tkinter import *
 from tkinter import filedialog
 from itertools import cycle
-from PIL import Image
-from PIL import ImageTk
 import os
+import time
 
-repertoire = "D:/Code/Python/cache"
+repertoire = "E:/"
 
 main = Tk()
 main.title("GraphicDir")
 try:
-    main.iconbitmap("Planning Requierement (optional)/write list.ico")
+    main.iconbitmap("icon.ico")
 except:
     print("L'icone de la fenêtre principale n'a pas été chargée")
 main.overrideredirect(1)
@@ -28,10 +27,22 @@ def CloseAll():
     main.destroy()
     sys.exit()
 
-def OverRiderFalse():
-    main.overrideredirect(0)
+counterfen = 1
+def OverRider():
+    global counterfen
+    if counterfen == 1:
+        main.overrideredirect(0)
+        counterfen = 0
+    else:
+        main.overrideredirect(1)
+        counterfen = 1
 
-def OverRiderTrue():
+def reduct(): 
+    main.overrideredirect(0)
+    main.state('iconic')
+    while  main.state() == 'iconic':
+        main.update()
+        time.sleep(0.1)
     main.overrideredirect(1)
 
 morePageCount = 1
@@ -49,7 +60,7 @@ def moreFichier():
 
 def file():
     def clicListbox(ListBox):
-        fileText = Text(backFrame,background="#0f0f0f", maxundo=-1, wrap='none',foreground="gray90",insertbackground='crimson',highlightthickness=0,borderwidth=0,selectbackground='#222222')
+        fileText = Text(backFrame,background="#0c0c0c", maxundo=-1, wrap='none',foreground="gray90",insertbackground='crimson',highlightthickness=0,borderwidth=0,selectbackground='#222222')
         fileText.place(x=200,y=30,width=large-200,height=haut-30)
         stockListbox = StringVar()
         stockListbox = listpara.get(listpara.curselection())
@@ -101,9 +112,9 @@ backFrame.place(x=0,y=0,width=large,height=haut)
 
 backFrameBar = Frame(main, bg='#111111')
 backFrameBar.place(x=0,y=0,width=large,height=30)
-reduire = Button(backFrameBar, text="━", relief = FLAT, bd = 0, bg = '#111111', fg = 'gray80', cursor="cross", activebackground='#534692')
+reduire = Button(backFrameBar, text="━", relief = FLAT, bd = 0, bg = '#111111', fg = 'gray80', cursor="cross", activebackground='#534692', command=reduct)
 reduire.place(x=large-150, y=0, width=50, height=30)
-augmenter = Button(backFrameBar, text="☐", relief = FLAT, bd = 0, bg = '#111111', fg = 'gray80', cursor="cross", activebackground='#634481')
+augmenter = Button(backFrameBar, text="☐", relief = FLAT, bd = 0, bg = '#111111', fg = 'gray80', cursor="cross", activebackground='#634481', command=OverRider)
 augmenter.place(x=large-100, y=0, width=50, height=30)
 fermer = Button(backFrameBar, text="✕", relief = FLAT, bd = 0, bg = '#111111', fg = 'gray80', cursor="cross", command=CloseAll, activebackground='crimson')
 fermer.place(x=large-50, y=0, width=50, height=30)
