f4c99342273675cc5656768ee56c8246ef89f36d
WarriorMachine <51814677+WarriorMachine@users.noreply.github.com>
2020-03-05 09:42:29 +0100

    Create dir s.py

---
+++ dir s.py
@@ -0,0 +1,14 @@
+from os.path import exists
+import os
+
+def drives():
+    drive_list = []
+    for drive in range(ord('A'), ord('Z')):
+        if exists(chr(drive) + ':'):
+            drive_list.append(chr(drive))
+    return drive_list
+
+for disk in drives():
+    for top, dirs, files in os.walk(str(disk+":/")):
+        for nm in files:
+            print(str(os.path.join(top, nm)))
