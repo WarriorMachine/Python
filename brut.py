173130cb7b5f05b2d5fe37f89a6cce51132bf729
WarriorMachine <51814677+WarriorMachine@users.noreply.github.com>
2020-04-29 15:59:54 +0200

    brutforce class

--- brut.py
+++ brut.py
@@ -1,20 +1,19 @@
 class Brut:
-    def __init__(self, characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/@?!:;.,~-", min=1, max=4, infos=1, wait_info=1):
-        if min > 0 or max > 0:
-            if min < max or min <= max:
-                if infos==1:
+    def __init__(self, characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789/@?!:;.,~-", min=1, max=4, infos=True):
+        self.characters, self.min, self.max, self.infos = characters, min, max, infos
+        if self.min > 0 or self.max > 0:
+            if self.min < self.max or self.min <= self.max:
+                if self.infos:
                     start_time = datetime.now()
-                for n in range(min, max+1):
-                    for i in itertools.product(characters, repeat=n):
+                for n in range(self.min, self.max+1):
+                    for i in itertools.product(self.characters, repeat=n):
                         chars = ''.join(i)
-                        print(chars)
-                if infos==1:
+                        print(chars) #edit this line as you want
+                if self.infos:
                     end_time = datetime.now()
                     print ('\n[BRUT] A commencer le : '+str(start_time))
                     print ('\n[BRUT] Fini le : '+str(end_time))	
                     print ('\n[BRUT] Temps total : '+str(end_time-start_time))
-                    if wait_info == 1:
-                        time.sleep(10)
             else:
                 print("\n[BRUT] Erreur 0002 : La valeur min doit être inférieur ou égal à la valeur max")
         else:
