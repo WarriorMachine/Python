0c495837d8c12abf4cf44b7f28b3e54278a27ed2
WarriorMachine <51814677+WarriorMachine@users.noreply.github.com>
2021-01-14 01:15:40 +0100

    create file

---
+++ getMissingNumberFromOne.py
@@ -0,0 +1,11 @@
+def getMissingNumbersFromOne(array : list): #return 0 if nothing miss
+		actual, missing, normal = sorted(array), [], []
+		if len(actual) < 1: missing.append(0)
+		else : [normal.append(x) for x in range(1, actual[-1]+1)]
+
+		for i, no in enumerate(normal):
+			if no not in actual: missing.append(no)
+
+		if len(missing) < 1: missing.append(0)
+
+		return missing
