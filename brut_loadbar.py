9c7b0b516832240f0d098e73f759148e4a4d2739
WarriorMachine <51814677+WarriorMachine@users.noreply.github.com>
2021-03-10 06:40:06 -0500

    upgrade loadbar

--- brut_loadbar.py
+++ brut_loadbar.py
@@ -3,19 +3,12 @@ def Brut(characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567
     for p in range(1, max+1-min+1):
         total =+ (len(characters)**p)
 
-    sys.stdout.write("Loading : |%s|" % (" " * lenght_bar))
-    sys.stdout.flush()
-    sys.stdout.write("\b" * (lenght_bar+1))
-
-    count = 0
+    count, progress = 0, 1
     for n in range(min, max+1):
         for i in itertools.product(characters, repeat=n):
-            chars = ''.join(i)
-            count += 1
+            chars, count = ''.join(i), count + 1
+            print(f"\r[{'▒'*progress}{' '*(lenght_bar-progress)}] | {round(progress/(lenght_bar+1)*100)}% | Try: {chars} |", flush=True, end='')
             #process
             if count == round(total/lenght_bar)-1:
-                sys.stdout.write("▒")
-                sys.stdout.flush()
-                count = 0
-    sys.stdout.write("| 100%")
-    sys.stdout.write("\nDone")
+                count, progress = 0, progress + 1
+    print("\nDone")
